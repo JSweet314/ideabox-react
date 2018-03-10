3e7f18648713e43cb98c7cc633fd42d8b1f10f15
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-09 17:32:27 -0700

    more tests

---
+++ src/__tests__/CardContainer.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import CardContainer from '../containers/CardContainer';
+
+describe('CardContainer', () => {
+  let cardContainer, idea1, idea2, visibleIdeas;
+
+  beforeEach(() => {
+    idea1 = {
+      id: 1234,
+      title: 'Hello',
+      body: 'World!',
+      quality: 'plausible'
+    };
+
+    idea2 = {
+      id: 4321,
+      title: 'New App!',
+      body: 'Woo Hoo',
+      quality: 'genius'
+    };
+
+    visibleIdeas = [idea1, idea2];
+    
+    cardContainer = shallow(
+      <CardContainer
+        visibleIdeas={visibleIdeas}
+        updateIdeaQuality={jest.fn()}
+        removeIdea={jest.fn()}
+        searchIdeas={jest.fn()}
+      />
+    );
+  });
+
+  it('should render without crashing', () => {
+    expect(cardContainer).toMatchSnapshot();
+
+    cardContainer = shallow(
+      <CardContainer
+        visibleIdeas={[]}
+        updateIdeaQuality={jest.fn()}
+        removeIdea={jest.fn()}
+        searchIdeas={jest.fn()}
+      />
+    );
+
+    expect(cardContainer).toMatchSnapshot();
+  });
+
+
+  it('should have a prop visibleIdeas as well as methods from the app component', () => {
+    expect(cardContainer.props('visibleIdeas')).toBeDefined();
+    expect(cardContainer.props('updateIdeaQuality')).toBeDefined();
+    expect(cardContainer.props('removeIdea')).toBeDefined();
+    expect(cardContainer.props('searchIdeas')).toBeDefined();
+  });
+});
---
+++ src/__tests__/IdeaCard.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import {IdeaCard} from '../IdeaCard.js';
+
+describe('IdeaCard', () => {
+  let ideaCard, idea;
+
+  beforeEach(() => {
+    idea = {
+      id: 1234,
+      title: 'Hello',
+      body: 'World!',
+      quality: 'plausible'
+    };
+
+    ideaCard = shallow(
+      <IdeaCard 
+        {...idea}
+        updateIdeaQuality={jest.fn()}
+        removeIdea={jest.fn()}
+      />
+    );
+  });
+
+  it('should render a card', () => {
+    expect(ideaCard).toMatchSnapshot();
+  });
+
+  it('should have props title, body, id, quality, updateIdeaQuality, and removeIdea', () => {
+    expect(ideaCard.props('title')).toBeDefined();
+    expect(ideaCard.props('body')).toBeDefined();
+    expect(ideaCard.props('id')).toBeDefined();
+    expect(ideaCard.props('quality')).toBeDefined();
+    expect(ideaCard.props('removeIdea')).toBeDefined();
+    expect(ideaCard.props('updateIdeaQuality')).toBeDefined();
+  });
+
+  it('should call update quality if the upvote or down vote buttons are clicked', () => {
+    ideaCard = mount(
+      <IdeaCard
+        {...idea}
+        updateIdeaQuality={jest.fn()}
+        removeIdea={jest.fn()}
+      />
+    );
+
+    const upVoteBtn = ideaCard.find('button.upVoteBtn');
+    const downVoteBtn = ideaCard.find('button.downVoteBtn');
+
+    upVoteBtn.simulate('click');
+    upVoteBtn.simulate('click');
+    
+    expect(ideaCard.props().updateIdeaQuality).toHaveBeenCalledTimes(2);
+  });
+
+  it('should call removeIdea when the delete button is clicked', () => {
+    ideaCard = mount(
+      <IdeaCard
+        {...idea}
+        updateIdeaQuality={jest.fn()}
+        removeIdea={jest.fn()}
+      />
+    );
+
+    const deleteBtn = ideaCard.find('button.deleteBtn');
+
+    deleteBtn.simulate('click');
+
+    expect(ideaCard.props().removeIdea).toHaveBeenCalledTimes(1);
+  });
+});
